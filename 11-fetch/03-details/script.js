e93d39a0d3e138c68eb6acf2ebcf7fc13feb6b57
leny <info@flatland.be>
2020-11-16 15:36:48 +0100

    Fix 11.3

--- 11-fetch/03-details/script.js
+++ 11-fetch/03-details/script.js
@@ -9,6 +9,34 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+const tpl = document.querySelector("#tpl-hero");
+const target = document.querySelector("#target");
+const input = document.querySelector("#hero-id");
+
 (() => {
-    // your code here
+    document.querySelector("#run").addEventListener("click", async () => {
+        const id = +input.value;
+
+        if (isNaN(id)) {
+            console.error("Invalid id!");
+            return;
+        }
+
+        try {
+            const response = await fetch(`http://localhost:3000/heroes/${id}`);
+            const {name, alterEgo, abilities} = await response.json();
+
+            const elt = tpl.cloneNode(true).content;
+
+            elt.querySelector(".name").innerText = name;
+            elt.querySelector(".alter-ego").innerText = alterEgo;
+            elt.querySelector(".powers").innerText = abilities.join(", ");
+
+            target.firstElementChild
+                ? target.replaceChild(elt, target.firstElementChild)
+                : target.appendChild(elt);
+        } catch (err) {
+            console.error(`Unknown hero with id: ${id}`);
+        }
+    });
 })();
