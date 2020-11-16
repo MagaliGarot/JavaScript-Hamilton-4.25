f235177c6773fadec81dd5720aeb4a3215abe88d
leny <info@flatland.be>
2020-11-16 14:56:09 +0100

    Fix 11.2

--- 11-fetch/02-list-to-template/script.js
+++ 11-fetch/02-list-to-template/script.js
@@ -9,6 +9,22 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+const tpl = document.querySelector("#tpl-hero");
+const target = document.querySelector("#target");
+
 (() => {
-    // your code here
+    document.querySelector("#run").addEventListener("click", async ()=>{
+        const response = await fetch("http://localhost:3000/heroes")
+        const heroes = await response.json();
+
+        heroes.forEach(({name, alterEgo, abilities})=>{
+            const elt = tpl.cloneNode(true).content;
+
+            elt.querySelector(".name").innerText = name;
+            elt.querySelector(".alter-ego").innerText = alterEgo;
+            elt.querySelector(".powers").innerText = abilities.join(", ");
+
+            target.appendChild(elt);
+        });
+    })
 })();
