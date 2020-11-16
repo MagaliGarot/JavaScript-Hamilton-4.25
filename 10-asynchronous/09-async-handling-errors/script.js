c22d2eefe9e2c427e9a8af88d722bf06b85fd9d0
leny <info@flatland.be>
2020-11-16 14:35:06 +0100

    Fix 10.9

--- 10-asynchronous/09-async-handling-errors/script.js
+++ 10-asynchronous/09-async-handling-errors/script.js
@@ -10,5 +10,12 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.querySelector("#run").addEventListener("click", async () => {
+        try {
+            const persons = await window.lib.getPersons();
+            console.log(persons);
+        } catch (error) {
+            console.error(error);
+        }
+    });
 })();
